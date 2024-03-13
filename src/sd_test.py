552e2aa480bf6ca2722416385962432eb09b26c8
Ben Schumacher <benschumacher0@gmail.com>
2024-03-13 15:49:44 +0800

    adjust for different server

--- src/sd_test.py
+++ src/sd_test.py
@@ -8,6 +8,9 @@ import torch
 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sd-turbo", torch_dtype=torch.float16, variant="fp16")
 pipe.to("cuda")
 
+# load lora file
+pipe.load_lora_weights("lora_test.safetensors")
+
 prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
 image = pipe(prompt=prompt, num_inference_steps=1, guidance_scale=0.0).images[0]
 
@@ -21,6 +24,6 @@ pipe = AutoPipelineForImage2Image.from_pretrained("stabilityai/sd-turbo", torch_
 pipe.to("cuda")
 
 init_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/cat.png").resize((512, 512))
-prompt = "cat wizard, gandalf, lord of the rings, detailed, fantasy, cute, adorable, Pixar, Disney, 8k"
+prompt = "a cat with a hat"
 
 image = pipe(prompt, image=init_image, num_inference_steps=2, strength=0.5, guidance_scale=0.0).images[0]
